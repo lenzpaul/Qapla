e6b123bfc4ca6fd89588cf1426e3b56af55727df
Lenz Thierry Paul <paullt@stumail.csci.viu.ca>
2020-04-03 01:17:08 -0700

    node types?

---
+++ dataStructures/functions.c
@@ -0,0 +1,76 @@
+
+#include <stdlib.h> 
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+#include "growingTree.c"
+
+void declareVar(struct DataNode var)
+{
+   //set children to 0
+   //set every field to 0 except name
+   //use constructNode(0);
+
+   //eg:
+   //declareVar alpha;
+   //
+   //struct DataNode *node = constructNode(0);
+   //node -> type = var->type;
+   //node -> name = var->name;
+   //HERE there should be a statement to assign alpha to mem
+}
+
+//for: string, boolean, integer or real
+void setConstant(struct DataNode con)
+{
+   //set children to 0
+   //set every field to 0 except name
+   //use constructNode(0);
+   
+   //eg: This could hold: 3
+   //
+   //struct DataNode *node = constructNode(0);
+   //node -> type = con->type;
+   //if(con->type = 1) node -> ival = con->ival;
+   //if(con->type = 2) node -> fval = con->fval;
+   //if(con->type = 3) node -> str = con->str;
+   //if(con->type = 4) node -> bval = con->bval;
+}
+
+
+void setOperator(char *operator, struct OpNode *op,
+   struct DataNode *leftOperand, struct DataNode *rightOperand)
+{
+   //create an OpNode and insert in parent 
+   op->leftOperand = leftOperand;
+   op->rightOperand = rightOperand;
+}
+
+
+
+/*
+struct DataNode* setOperator(char* operator, struct OpNode *op, 
+   struct DataNode leftOperand, struct DataNode rightOperand)
+{
+
+   strcpy(op->operator,operator);
+   int lot = leftOperand.dtype ;
+   int rot = rightOperand.dtype;
+
+   struct DataNode *node = constructNode(1) ;
+
+   if(strcmp(operator, "+") == 0)
+   {
+         if(lot==1 && rot==1){ //int
+            node->dtype = 1;
+            node->ival = leftOperand.ival + rightOperand.ival;
+            
+         }
+         //if(lot==2 && rot==2 || lot==1 && lot==2 || lot==2 && rot==1) //float
+   }         
+
+   return node;
+         
+}
+*/
--- dataStructures/growingTree.c
+++ dataStructures/growingTree.c
@@ -1,41 +1,56 @@
 
 #include <stdlib.h> 
 #include <stdio.h>
+#include <stdbool.h>
 
-struct Node {
+struct OpNode {
+   char operator[5] ; //the operator
+   struct DataNode* leftOperand;
+   struct DataNode* rightOperand;
+};
+
+//default container for IDENTIFIERs, INTEGERs, REALs, 
+//STRINGs, BOOLs, and FUNCTIONs 
+struct DataNode {
+   struct DataNode *parent;
+   struct DataNode **children ;
    size_t size;
    size_t capacity;
 
-   struct Node *parent;
-   //array of children ptr
-   struct Node **children ;
-} ;//*root;
+   long ival, dtype; 
+   double fval;
+   char str[4096], name[256]; 
+   bool bval;
+} ;
 
-struct Node* constructNode(size_t capacity){
-   struct Node* node;
+//expects a capacity, returns the new node
+struct DataNode* constructNode(size_t capacity){
+   struct DataNode* node;
    //allocate memory for new node
-   node = malloc(sizeof(struct Node));
-
-   node->children = malloc(capacity * sizeof(struct Node));
+   node = malloc(sizeof(struct DataNode));
+   
+   if(capacity > 0)
+      node->children = malloc(capacity * sizeof(struct DataNode));
+   
    node->size = 0;
    node->capacity = capacity;
 
    return node;
 }
 
-void insertChild(struct Node *node, struct Node *element){
+void insertChild(struct DataNode *node, struct DataNode *element){
    //grow if necessary
    if (node->size == node->capacity) {
       node->capacity *= 2;
       node->children = realloc(node->children, 
-            node->capacity * sizeof(struct Node));
+            node->capacity * sizeof(struct DataNode));
    }
    //insert
    node->children[node->size++] = element;
 
 }
 
-void freeNode(struct Node *node) {
+void freeNode(struct DataNode *node) {
    //free each innder node
    for(int i=node->size-1; i>=0; i--){
       free(node->children[i]);
@@ -44,37 +59,35 @@ void freeNode(struct Node *node) {
    //free array of nodes
    free(node->children);
    node->children = NULL;
-   
+
    //free root
    free(node);
    node->size = node->capacity = 0;
    node = NULL;
 }
 
-/*
 
-int main()
-{
+   int main()
+   {
 
-   
-   //construct root with 1 child capacity
-   struct Node *root = constructNode(1);
-   struct Node *node1 = constructNode(1);
-   struct Node *node2 = constructNode(1);
-   struct Node *node3 = constructNode(1);
 
-   //create nodes and insert as child to root
-   insertChild(root, node1);
-   insertChild(root, node2);
-   insertChild(root, node3);
-   insertChild(root, constructNode(1));
-   insertChild(root, constructNode(1));
-   
-   root->children[3]->size = 4;
-   root->children[3]->size = 4;
-   //print new size and capacity
-   printf("%d\n", root->size);
-   printf("%d\n", root->capacity);
-   freeNode(root);
+//construct root with 1 child capacity
+struct DataNode *root = constructNode(1);
+struct DataNode *node1 = constructNode(1);
+struct DataNode *node2 = constructNode(1);
+struct DataNode *node3 = constructNode(1);
+
+//create nodes and insert as child to root
+insertChild(root, node1);
+insertChild(root, node2);
+insertChild(root, node3);
+insertChild(root, constructNode(1));
+insertChild(root, constructNode(1));
+
+root->children[3]->size = 4;
+root->children[3]->size = 4;
+//print new size and capacity
+printf("%d\n", root->size);
+printf("%d\n", root->capacity);
+freeNode(root);
 }
-*/
