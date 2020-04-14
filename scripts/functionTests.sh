4347fe463bd7c582d980c48193afda5ed44f3587
Lenz Thierry Paul <paullt@stumail.csci.viu.ca>
2020-04-14 07:42:07 -0700

    Cleaning and implementing smaller missing pieces

---
+++ scripts/functionTests.sh
@@ -0,0 +1,6 @@
+#!/usr/local/bin/bash
+./fooChecker < valid/print/t0 #assign a var
+./fooChecker < valid/print/t1 #print int
+./fooChecker < valid/print/t2 #print float
+./fooChecker < valid/print/t3 #print string
+./fooChecker < valid/print/t4 #print bool
---
+++ scripts/showbugs.sh
@@ -0,0 +1,2 @@
+#! /usr/local/bin/bash
+grep BUG /home/student/paullt/csci330/project/notes/bugReport.bug
